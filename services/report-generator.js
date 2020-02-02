dab8b7e106644f2170b823e1cf3c8feb6f51ced6
Artem Kozel <kozel.as@ya.ru>
2020-02-02 22:58:56 +0300

    Report. Updated generation for many categories.

--- services/report-generator.js
+++ services/report-generator.js
@@ -1,12 +1,27 @@
 const xlsx = require('xlsx');
-const moment = require('moment');
 const { Op } = require('sequelize');
 
-const { Item, ItemDistribution, DistributionPlace } = require('../models');
+const { Item, ItemDistribution, DistributionPlace, sequelize } = require('../models');
 
 const CATEGORY_ID = 6;
 
+const getAllCategoryIdsByParent = async () => {
+  const query = `
+    select id from (select * from Categories order by parentId, id) products_sorted,
+    (select @pv := '${CATEGORY_ID}') initialisation
+    where find_in_set(parentId, @pv)
+    and length(@pv := concat(@pv, ',', id))
+  `;
+
+  const results = await sequelize.query(query);
+  const ids = [].concat(...results).map(result => result.id).concat(CATEGORY_ID);
+
+  return [...new Set(ids)];
+}
+
 const loadDataForReport1 = async (start, end) => {
+  const ids = await getAllCategoryIdsByParent();
+  
   const query = {
     attributes: ['id', 'date', 'amount'],
     include: [
@@ -20,7 +35,7 @@ const loadDataForReport1 = async (start, end) => {
         as: 'item',
         attributes: ['id', 'name', 'amount'],
         where: {
-          categoryId: CATEGORY_ID,
+          categoryId: ids,
         },
       },
     ],
@@ -40,7 +55,6 @@ const loadDataForReport1 = async (start, end) => {
 
 const createExcelReport = async (start, end) => {
   const data = await loadDataForReport1(start, end);
-
   const wb = xlsx.utils.book_new();
 
   wb.SheetNames.push('Расходники');
@@ -49,29 +63,66 @@ const createExcelReport = async (start, end) => {
     'Наименование',
     'Место списания',
     'Дата списания',
-    'Количество',
+    'Кол-во',
+    'Общее кол-во'
   ];
 
-  const result = data.map(value => {
-    return [
-      value.item.name,
-      value.place.name,
-      value.date,
-      value.amount,
-    ];
-  });
+  const groupedByItemName = data
+    .reduce((acc, value) => {
+      const objValue = value.toJSON();
+
+      acc[objValue.item.name] = acc[objValue.item.name] || [];
+      acc[objValue.item.name].push(objValue);
+
+      return acc;
+    }, {});
+
+  const result = [];
+  const merges = [];
+  let startRowInd = 0;
+
+  Object.keys(groupedByItemName)
+    .forEach(key => {
+      const items = groupedByItemName[key];
+      let mergeName = {
+        s: { r: startRowInd + 1, c: 0 },
+        e: { r: startRowInd + items.length, c: 0 },
+      };
+      let mergeTotalAmount = {
+        s: { r: startRowInd + 1, c: 4 },
+        e: { r: startRowInd + items.length, c: 4 },
+      };
+      startRowInd += items.length;
+      
+      merges.push(mergeName);
+      merges.push(mergeTotalAmount);
+
+      const totalAmount = items.reduce((acc, item) => acc + item.amount, 0);
+
+      items.forEach((value, i) => {
+        result.push([
+          i === 0 ? [key] : '',
+          value.place.name,
+          value.date,
+          value.amount,
+          i === 0 ? totalAmount : '',
+        ]);
+      });
+    });
 
   result.unshift(fields);
   const ws = xlsx.utils.aoa_to_sheet(result);
 
   const wscols = [
-    { wch:70 },
-    { wch:20 },
-    { wch:20 },
-    { wch:20 },
+    { wch: 70 },
+    { wch: 20 },
+    { wch: 13 },
+    { wch: 7 },
+    { wch: 13 },
   ];
 
-  ws['!cols'] = wscols;
+  ws['!cols'] = wscols;  
+  ws['!merges'] = merges;
 
   wb.Sheets['Расходники'] = ws;
 
