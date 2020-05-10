134ca9713f16851a21d707fd177f52ecb7cb43ee
Artem Kozel <kozel.as@ya.ru>
2020-05-10 19:33:56 +0300

    Distribution. Refactored response data.

--- routes/distribution.js
+++ routes/distribution.js
@@ -4,18 +4,12 @@ const ctrl = require('../controllers/itemDistribution');
 
 const { loadItemDistribution } = require('../middleware/loader');
 
-router.get('/item/:itemId/distribution/:id', ctrl.getInfo);
-router.post('/item/:itemId/distribution', ctrl.create);
-router.put('/item/:itemId/distribution/:id', loadItemDistribution, ctrl.update);
-
-router.delete('/item/:itemId/distribution/:id', loadItemDistribution, ctrl.remove);
-
 router.get('/distribution/places', ctrl.getPlaces);
 router.post('/distribution/places', ctrl.createPlace);
 
-router.get('/distribution/places/:placeId/service', ctrl.getPlaceServices);
-router.post('/distribution/places/:placeId/service', ctrl.createPlaceService);
-router.put('/distribution/places/:placeId/service/:id', ctrl.updatePlaceService);
-router.delete('/distribution/places/:placeId/service/:id', ctrl.removePlaceService);
+router.get('/distribution/:id', ctrl.getInfo);
+router.post('/distribution', ctrl.create);
+router.put('/distribution/:id', loadItemDistribution, ctrl.update);
+router.delete('/distribution/:id', loadItemDistribution, ctrl.remove);
 
 module.exports = router;
